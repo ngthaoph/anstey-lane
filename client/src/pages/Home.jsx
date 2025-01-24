7eb721485bc2820d2cc748b28dcdeed59b3151e4
Thao Nguyen <ngthaoph@gmail.com>
2025-01-24 18:13:26 +1100

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,8 +1,7 @@
 import { Fragment } from "react";
 import Container from "react-bootstrap/Container";
 import Carousel from "../components/layout/Carousel";
-import AlButton from ".././components/common/AlButton";
-import wave from "../assets/images/wave.svg";
+
 const Home = () => {
   return (
     <Fragment>
